adef875c6cc68d7d6400ee2daf3c53ede87a7ecc
Lee Joonhun <jhlee63@outlook.com>
2022-03-02 15:55:25 +0900

    Upload trainval datasets

--- ljh/config.py
+++ ljh/config.py
@@ -2,4 +2,4 @@ S, B, C = 7, 2, 20  # num_grid, num_bbox, num_class
 
 lambda_coord, lambda_noobj = 5, 0.5
 
-data_dir = "/data/torch/VOCdetection"
+data_dir = "~/data/torch/VOCdetection"
--- ljh/train.py
+++ ljh/train.py
@@ -5,6 +5,15 @@ ljh_dir = str(Path(__file__).parent)
 sys.path.append(ljh_dir)
 
 import torch
-from utils import *
-from dataset import CustomDataset, CustomDataLoader
+from torchvision.datasets import VOCDetection
+from torch.utils.data import DataLoader
 from model import YOLOv1
+from utils import *
+
+data_dir = "~/data/torch/VOCDetection"
+
+train_dataset07 = VOCDetection(data_dir, "2007", "train")
+valid_dataset07 = VOCDetection(data_dir, "2007", "val")
+
+train_dataset12 = VOCDetection(data_dir, "2012", "train")
+valid_dataset12 = VOCDetection(data_dir, "2012", "val")
